0f20aa28dc76fc269e43c4259d0f08296aa750a0
JSantangelo83 <joakinsant@gmail.com>
2022-10-03 04:13:31 -0300

    Added bootstrap, and fix

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,18 +1,13 @@
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
+import { Col, Container, Row } from 'react-bootstrap';
 export default function Home() {
   return (
-    <Router>
-      <div>
-        <Link to='/Ruta1'>Ruta 1</Link>
-      </div>
-      <div>
-        <h1>Aca se Renderizan las rutas: </h1>
-        <Routes>
-          <Route path='/Ruta1' element={Ruta1} />
-        </Routes>
-      </div>
-    </Router>
+    <Container fluid>
+      <Row>
+        <Col>Hola</Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ThemeProvider } from 'react-bootstrap';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Home />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+    />
+    <ThemeProvider
+      breakpoints={['sm', 'md', 'lg']}
+      minBreakpoint='sm'
+    >
+      <Home />
+    </ThemeProvider>;
   </React.StrictMode>
 );
